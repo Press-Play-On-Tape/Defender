1838a999cd9087ab1ebcdd530363da9a02fd5765
Filmote <simon@bloggingadeadhorse.com>
2024-03-03 17:02:40 +1100

    Save Work

--- Defender-UpdateEntities.ino
+++ Defender-UpdateEntities.ino
@@ -12,109 +12,129 @@ void updatePlayer(uint8_t frameCount) {
 
 void updateCamera(uint8_t frameCount) {
 
-    int16_t x = player.getX() - camera.getX();
+//     int16_t x = player.getX() - camera.getX();
 
-Serial.print("x = ");
-Serial.println(x);
+// Serial.print("x = ");
+// Serial.println(x);
 
-    switch (player.getAccelerationIdxX()) {
+//     switch (player.getAccelerationIdxX()) {
 
-        case Constants::Player_Acceleration_Left_Max ... Constants::Player_Acceleration_Left_Min:
+//         case Constants::Player_Acceleration_Left_Max ... Constants::Player_Acceleration_Left_Min:
 
-            if (player.isDecceleratingX()) {
+//             if (player.isDecceleratingX()) {
 
-                if (x >= Constants::Player_Offset_X_Mid) {
-Serial.println("A1");
-                    camera.incX(Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
+//                 if (x >= Constants::Player_Offset_X_Mid) {
+// Serial.println("A1");
+//                     camera.incX(Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
 
-                }
+//                 }
 
-            }
-            else {
+//             }
+//             else {
 
-                if (x <= Constants::Player_Offset_X_Max) {
+//                 if (x <= Constants::Player_Offset_X_Max) {
 
-                    switch (player.getAccelerationIdxX()) {
+//                     switch (player.getAccelerationIdxX()) {
 
-                        case 0 ...11:
-Serial.println("A2");
-                            camera.incX(-Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
-                            break;
+//                         case 0 ...11:
+// Serial.println("A2");
+//                             camera.incX(-Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
+//                             break;
 
-                        case 12 ... 13:
+//                         case 12 ... 13:
 
-                            if (x <= Constants::Player_Offset_X_Mid) {
-Serial.println("A3");
-                                camera.incX(-2);
-                            }
+//                             if (x <= Constants::Player_Offset_X_Mid) {
+// Serial.println("A3");
+//                                 camera.incX(-2);
+//                             }
 
-                            break;
+//                             break;
 
-                    }
+//                     }
 
-                }
+//                 }
 
-            }
+//             }
 
-            break;
+//             break;
 
-        case Constants::Player_Acceleration_Stationary:
+//         case Constants::Player_Acceleration_Stationary:
 
-            if (x < Constants::Player_Offset_X_Mid) {
-Serial.println("A4");
-                camera.incX(-32);
-            }
-            else if (x > Constants::Player_Offset_X_Mid) {
-Serial.println("A5");
-                camera.incX(32);
-            }
+//             if (x < Constants::Player_Offset_X_Mid) {
+// Serial.println("A4");
+//                 camera.incX(-32);
+//             }
+//             else if (x > Constants::Player_Offset_X_Mid) {
+// Serial.println("A5");
+//                 camera.incX(32);
+//             }
 
-            break;
+//             break;
 
 
-        case Constants::Player_Acceleration_Right_Min ... Constants::Player_Acceleration_Right_Max:
+//         case Constants::Player_Acceleration_Right_Min ... Constants::Player_Acceleration_Right_Max:
 
-            if (player.isDecceleratingX()) {
+//             if (player.isDecceleratingX()) {
 
-                if (x <= Constants::Player_Offset_X_Mid) {
-Serial.println("A6");
+//                 if (x <= Constants::Player_Offset_X_Mid) {
+// Serial.println("A6");
 
-                    camera.incX(-Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
+//                     camera.incX(-Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
 
-                }
+//                 }
 
-            }
-            else {
+//             }
+//             else {
 
-                if (x >= Constants::Player_Offset_X_Min) {
+//                 if (x >= Constants::Player_Offset_X_Min) {
 
-                    switch (player.getAccelerationIdxX()) {
+//                     switch (player.getAccelerationIdxX()) {
 
-                        case 18 ... 28:
-Serial.println("A7");
-                            camera.incX(Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
-                            break;
+//                         case 18 ... 28:
+// Serial.println("A7");
+//                             camera.incX(Constants::Camera_Offset_X[player.getAccelerationIdxX()]);
+//                             break;
 
-                        case 15 ... 16:
+//                         case 15 ... 16:
 
-                            if (camera.getX() <= Constants::Player_Offset_X_Mid) {
-Serial.println("A8");
-                                camera.incX(2);
-                            }
+//                             if (camera.getX() <= Constants::Player_Offset_X_Mid) {
+// Serial.println("A8");
+//                                 camera.incX(2);
+//                             }
 
-                            break;
+//                             break;
 
-                    }
+//                     }
 
-                }
+//                 }
 
-            }
+//             }
 
-            break;
+//             break;
 
-    }
+//     }
 
+    float tcamx = player.getX() - CAMERA_OFFSET_X;
+    float xmax = 1000 * 16 - 129;
+
+    tcamx += player.getAccelerationX() * (CAMERA_OFFSET_X_VEL_DELTA / PLAYER_MAX_MOVE_VEL_X);
+
+    if (player.getDirectionX() == Direction::Right)
+        tcamx += CAMERA_OFFSET_X_DELTA;
+    else
+        tcamx -= CAMERA_OFFSET_X_DELTA;
+    
+    if(tcamx < 0) tcamx = 0;
+    if(tcamx > xmax) tcamx = xmax;
+
+    float d2camx =
+        (tcamx - camera.getX()) * CAMERA_UPDATE_X_ALPHA -
+        camera.getVelocityX() * CAMERA_UPDATE_X_BETA;
+
+    camera.incX(camera.getVelocityX() * CAMERA_UPDATE_X_DT);
+    camera.incVelocityX(d2camx * CAMERA_UPDATE_X_DT);
 }
+
 
 
 void updateBullets(int16_t player_x) {
--- src/entities/Camera.h
+++ src/entities/Camera.h
@@ -9,18 +9,27 @@ class Camera {
     private:
 
         int16_t x = 0;
+        int16_t vx = 0;
         
     public:
 
         int16_t getX()                                  { return this->x; }        
+        int16_t getVelocityX()                          { return this->vx; }        
 
         void setX(int16_t val)                          { this->x = val; }
+        void setVelocityX(int16_t val)                  { this->vx = val; }
 
     public:
 
         void incX(int8_t val) {
         
             this->x = this->x + val;
+        
+        }
+
+        void incVelocityX(int8_t val) {
+        
+            this->vx = this->vx + val;
         
         }
 
