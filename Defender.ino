133a6a8bc614f867ab8b5ceb0e4a9f6981b4a732
Filmote <simon@bloggingadeadhorse.com>
2024-03-16 13:52:04 +1100

    Save Work

--- Defender.ino
+++ Defender.ino
@@ -48,6 +48,10 @@ uint8_t frameCount = 0;
 bool particlesNeedRendering = false;
 bool scoresNeedRendering = false;
 uint8_t titleCounter = 0;
+Enemy *enemyPickup = nullptr;
+uint8_t health = Constants::HealthMax;
+uint8_t healthBlink = 0;
+uint8_t treasureCount = Constants::TreasureCount;
 
 
 void setup() {
